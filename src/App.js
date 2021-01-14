37787cd33eb0437db33f59b198f6fa55ebf69ed9
Iyin Oluleke <iyinoluwa.olulek@gmail.com>
2021-01-14 17:38:54 +0100

    create the setup for the website

---
+++ src/App.js
@@ -0,0 +1,14 @@
+//import React fom "react";
+import Header from "./components/sections/Header/Header";
+import Home from "./components/Home/Home";
+
+const App = () => {
+  return(
+    <div>
+      <Header/>
+      <Home/>
+    </div>
+  )
+}
+ 
+export default App;
---
+++ src/components/sections/Header/Header.js
@@ -0,0 +1,15 @@
+//import React from "react";
+import "./Header.css";
+
+const Header = () => {
+	return(
+		<div className= "movie-header">
+			<div className="movie-content">
+			<h2 className="movie-logo">CLEARMOVIES</h2>
+			</div>
+
+		</div>
+	)
+}
+
+export default Header;
