5538ad7c052cb5650ef5a10ba2721bf02de6fde4
Iyin Oluleke <iyinoluwa.olulek@gmail.com>
2021-01-22 01:43:59 +0100

    Fetch data from the API

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { API_URL, API_KEY, IMAGE_BASE_URL, BACKDROP_SIZE, POSTER_SIZE} from "../../config";
 import "./Home.css";
 import SearchBar from "../sections/SearchBar/SearchBar";
 import HeroImage from "../sections/HeroImage/HeroImage";
@@ -8,7 +9,26 @@ import LoadMoreBtn from "../sections/LoadMoreBtn/LoadMoreBtn"
 
 class Home extends Component{
 	state = {
+		movies: [],
+		heroImage: null,
+		loading: false,
+		currentPage: 0,
+		totalPages: 0,
+		searchTerm: ""
+	}
 
+	componentDidMount(){
+		this.setState({loading:true});
+		const endpoint ="${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1";
+		this.fetchItems(endpoint);
+	}
+
+	fetchItems = (endpoint) =>{
+		fetch(endpoint)
+		.then(result => result.json())
+		.then(result => {
+			console.log(result);
+		})
 	}
 	render(){
 		return(
